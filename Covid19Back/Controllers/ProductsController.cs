869b86545bd1a0665e6319e8d3feadfe5490504b
Dimka_ Dimka_ <dimka05023@gmail.com>
2020-03-23 17:55:02 +0200

    add product controller

---
+++ Covid19Back/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Covid19Back.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Covid19Back.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult GetAll()
+        {
+            List<ProductDTO> model = new List<ProductDTO>() {
+            new ProductDTO{
+                title = "Salo",
+                price = "99999",
+                url = "/android/salo1.jfif"
+
+
+            }
+            };
+            return Ok(model);
+        }
+        
+    }
+}
---
+++ Covid19Back/DTO/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid19Back.DTO
+{
+    public class ProductDTO
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string price { get; set; }
+    }
+}
